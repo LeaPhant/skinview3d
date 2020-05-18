fb56a070a1b0f31b11f929e4808d04745f6c1f04
LeaPhant <mail@lea.moe>
2020-05-18 20:05:56 +0200

    Add skinview-utils to repo

---
+++ src/utils/types.d.ts
@@ -0,0 +1,3 @@
+export declare type TextureCanvas = HTMLCanvasElement | OffscreenCanvas;
+export declare type TextureSource = HTMLImageElement | HTMLVideoElement | ImageBitmap | TextureCanvas;
+export declare type ModelType = "default" | "slim";
